6023dbaad2b96ccd0160c2bf4a03e4eba51e8b06
Paula Andrea Molina <paulamolina983@gmail.com>
2023-09-08 16:04:19 -0300

    Se agrega la función ejemplares_prestados y eliminar_ejemplar_libro, al menú principal

--- bibloteca.py
+++ bibloteca.py
@@ -10,6 +10,22 @@ libros.append(l.libro3)
 
 def ejemplares_prestados():
     # Opción 5 del menú ppal - Visualiza la cantidad de libros prestados de cada uno
+    print("*****************************************************************************************************************************")
+    print("Código  Título/Nombre                                        Autor                Nº libros prestados      Nº total de libros")
+    print("*****************************************************************************************************************************")
+    for libro in libros:
+        codigo=libro["cod"]
+        cantidad=libro["cant_ej_ad"]
+        cant_prestados=libro["cant_ej_pr"]
+        nombre=libro["titulo"]
+        autor=libro["autor"]
+
+        if cant_prestados==0:
+            print("-----------------------------------------------------------------------------------------------------------------------------")
+            print(f"El libro {nombre}, cuyo código es {codigo}, no tiene préstamos registrados.")
+            print("-----------------------------------------------------------------------------------------------------------------------------")
+        else:
+            print(f"{codigo} {nombre} {autor} {cant_prestados} {cantidad}")
     return None
 
 def registrar_nuevo_libro():
@@ -25,6 +41,19 @@ def registrar_nuevo_libro():
 
 def eliminar_ejemplar_libro():
     # Opción 4 del menú principal - "Eliminar ejemplar"
+    codigo_a_borrar=input("Por favor ingrese el código a eliminar: ")
+    for libro in libros:
+        if codigo_a_borrar==libro["cod"]:
+            if libro["cant_ej_ad"]==0:
+                print("No hay ejemplares para dar de baja. Se han prestado todos los ejemplares de este libro o se dieron de baja todos los libros, por favor verifique.")
+            else:
+                stock=libro["cant_ej_ad"]
+                print(f"Se ha quitado una unidad del stock total de ese libro que era de {stock} unidades. *Código del libro: {codigo_a_borrar}")
+                libro["cant_ej_ad"]-=1
+                print("Total actualizado de unidades en stock de ese libro es: ")
+                print(libro["cant_ej_ad"])
+            return
+    print(f"El código ingresado: {codigo_a_borrar} no se encuentra asignado a ningún libro existente, verifique.\n")
     return None
 
 def prestar_ejemplar_libro():
