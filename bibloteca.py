3604a9f733fe029c3fda66a202dbfaca0ffbacd9
Enrique Marconi Favini <enriquefavini@hotmail.com>
2023-09-08 14:51:58 -0300

    Se completan las opciones del menú correspondientes al ALTA y PRESTAMO de un libro

--- bibloteca.py
+++ bibloteca.py
@@ -9,24 +9,45 @@ libros.append(l.libro2)
 libros.append(l.libro3)
 
 def ejemplares_prestados():
-    # completar
+    # Opción 5 del menú ppal - Visualiza la cantidad de libros prestados de cada uno
     return None
 
 def registrar_nuevo_libro():
-    nuevo_libro = l.nuevo_libro()
-    #completar
+    # Opción 3 del menú principal - "Registrar nuevo libro"
+    alta_nuevo_libro=l.nuevo_libro()    # Carga de datos realizada en la función nuevo_libro() ubicada en libro.py
+    if alta_nuevo_libro:                # si no viene vacío
+        libros.append(alta_nuevo_libro)
+        print("")
+        print("Los nuevos libros han sido agregados al registro de la biblioteca")
+    else:
+        print("No se realizó ningún alta por elección del usuario")
     return None
 
 def eliminar_ejemplar_libro():
-    #completar
+    # Opción 4 del menú principal - "Eliminar ejemplar"
     return None
 
 def prestar_ejemplar_libro():
-    #completar
+    # Opción 1 del menú - Presta un libro - Resta uno al stock de libros prestados
+    codigo_a_buscar=input("Por favor ingrese el código del libro que a prestar: ")
+    for libro in libros:
+        if codigo_a_buscar==libro["cod"]:
+            if libro["cant_ej_ad"]==0:
+                print("Lamentablemente no hay stock de ese libro, se han prestado todos los ejemplares disponibles")
+            else:
+                libro["cant_ej_ad"]-=1
+                print(f"El stock de libros del código: {codigo_a_buscar} fue actualizado, ahora hay disponibles para prestar un total de: ")
+                print(libro["cant_ej_ad"])
+                libro["cant_ej_pr"]+=1
+                print(f"Los prestamos totales efectuados del código: {codigo_a_buscar} es de: ")
+                print(libro["cant_ej_pr"])
+            return
+    print(f"El código ingresado: {codigo_a_buscar} no se encuentra asignado a ningún libro existente, verifique.\n")
+    
     return None
 
 def devolver_ejemplar_libro():
-    #completar
+    # Opción 2 del menú - Devuelve un libro - Agregar uno al stock de libros prestados
     return None
 
 def nuevo_libro():
